b4945d6b3e5bc04ac298d43b85c2b26e61a1287a
Andrey <gordienkoas@.bk.ru>
2024-11-25 17:11:11 +0300

    remake welcome_user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,6 @@
+import prompt
+
 def welcome_user():
-    global name
-    print('May I have your name? ', end='')
-    name = str(input())
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     return name
