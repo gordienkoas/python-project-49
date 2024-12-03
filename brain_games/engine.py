b48e0ee2693d1586d69c9709fff13c987862e3ef
user <gordienkoas@bk.ru>
2024-12-03 14:56:06 +0300

    install isort

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -3,20 +3,22 @@ import prompt
 
 
 def play(game_module):
-    name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
-    print(f'Hello, {name}\n{game_module.DESCRIPTION}')
+    name = prompt.string("Welcome to the Brain Games!\nMay I have your name? ")
+    print(f"Hello, {name}\n{game_module.DESCRIPTION}")
 
     for _ in range(ROUNDS):
         question, correct_answer = game_module.generate_question_and_answer()
 
-        print(f'Question: {question}')
-        answer = prompt.string('Your answer: ')
+        print(f"Question: {question}")
+        answer = prompt.string("Your answer: ")
 
         if answer == correct_answer:
-            print('Correct!')
+            print("Correct!")
         else:
-            print(f"{answer} is wrong answer ;(."
-                  f"Correct answer was {correct_answer}"
-                  f"\nLet's try again, {name}!")
+            print(
+                f"{answer} is wrong answer ;(."
+                f"Correct answer was {correct_answer}"
+                f"\nLet's try again, {name}!"
+            )
             return
-    print(f'Congratulations, {name}!')
+    print(f"Congratulations, {name}!")
